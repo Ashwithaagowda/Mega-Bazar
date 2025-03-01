d4e65c11d1d7c98736de5f58e81831ad5612b199
Ashwithaagowda <ashwithagowda22@gmail.com>
2025-03-01 12:00:06 +0530

    change in error component

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -7,7 +7,7 @@ export default function Error() {
     
    <img src= "https://tse3.mm.bing.net/th?id=OIP.WHVbZnI9AZJw1phnLcSNxAHaDX&pid=Api&P=0&h=180"
     alt=""/>
-  
+  <h1>hii</h1>
   </div>
   );
 }
