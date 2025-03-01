703d26499c5ca6aae776a810786c652286d65270
Ashwithaagowda <ashwithagowda22@gmail.com>
2025-03-01 11:56:19 +0530

    change in error component

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,9 +4,9 @@ export default function Error() {
   return(
 
   <div>
-    404 page not found
-   <img src= "https://tse3.mm.bing.net/th?id=OIP.WHVbZnI9AZJw1phnLcSNxAHaDX&pid=Api&P=0&h=180"/>
-
+    
+   <img src= "https://tse3.mm.bing.net/th?id=OIP.WHVbZnI9AZJw1phnLcSNxAHaDX&pid=Api&P=0&h=180"
+    alt=""/>
   </div>
   );
 }
